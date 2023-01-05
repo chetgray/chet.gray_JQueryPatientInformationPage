81ddd53897e0fb7036959627e152bc54f2429842
Chet Gray <chet.gray@waystar.com>
2023-01-05 14:59:12 -0500

    ♻ refactor: toggle styled class to toggle display

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 $(function () {
-    $(".form__optional-section").hide();
+    $(".form__optional-section").addClass("form__optional-section--hidden");
 
     // toggle primary insurance section based on checkbox
     $("#has-primary-insurance").on("change", function () {
-        $("#primary-insurance-info").toggle();
+        $("#primary-insurance-info").toggleClass("form__optional-section--hidden");
     });
 
     // toggle secondary insurance section based on checkbox
     $("#has-secondary-insurance").on("change", function () {
-        $("#secondary-insurance-info").toggle();
+        $("#secondary-insurance-info").toggleClass("form__optional-section--hidden");
     });
 
     // toggle text alert section based on checkbox
     $("#has-text-alert").on("change", function () {
-        $("#text-alert-info").toggle();
+        $("#text-alert-info").toggleClass("form__optional-section--hidden");
     });
 });
